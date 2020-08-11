f65dead42c2168be44f855b6119b4c96cac4c579
Triana Bares <triaabares@gmail.com>
2020-08-11 14:08:15 -0300

    login update 2

--- js/login.js
+++ js/login.js
@@ -1,6 +1,3 @@
-function toIndex() { 
-        location.href = "index.html";
-}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
