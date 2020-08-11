ddf71f715a18511dd46b06964d6e42aea510ecea
Triana Bares <triaabares@gmail.com>
2020-08-11 05:35:09 -0300

    login y ptoducts

--- js/login.js
+++ js/login.js
@@ -1,6 +1,13 @@
+function validar()  { 
+        window.location="index.html";;
+}
+ 
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    
 });
