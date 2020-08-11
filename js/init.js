26f865897b89acecd99b8a33b3c86e243fe37ae4
Triana Bares <triaabares@gmail.com>
2020-08-11 08:34:49 -0300

    prueba6

--- js/init.js
+++ js/init.js
@@ -40,6 +40,12 @@ var getJSONData = function(url){
     });
 }
 
+function toLogin() {
+  if(!toIndex()){
+  location.href = "login.html"; 
+}
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
